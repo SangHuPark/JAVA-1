5102e8186fdf02bb661d3db2af00a28b48079b21
SangHuPark <tkdgn5408407@gmail.com>
2023-07-28 16:20:15 +0900

    Delete Programming directory

--- Programming/ClassApp.java
+++
@@ -1,9 +0,0 @@
-public class ClassApp {
-
-	public static void main(String[] args) {
-		System.out.println(Math.PI);
-		System.out.println(Math.floor(1.6));
-		System.out.println(Math.ceil(1.6)); // Class 란 서로 연관된 변수와 메서드를 모아서 이름을 붙인 것
-	}
-
-}
